fc9a0bb1a7e1640288ce89007f5c7be9f3e70d71
VíctorManuel <manuelluoficial@gmail.com>
2024-10-02 17:38:51 +0200

    commit nurse filter

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -12,26 +12,27 @@ class NurseController extends AbstractController
     #[Route('/nurse', name: 'app_nurse')]
     public function index(): JsonResponse
     {
-        $json_nurse = file_get_contents('C:\Users\VíctorManuelLucumíGr\Documents\Symfony\ejemple_projecte\src\Controller\DATA.json');
+        $json_nurse = file_get_contents('DATA.json');
         $json_data = json_decode($json_nurse, true);
         // Retorna los datos como una respuesta JSON
         return new JsonResponse($json_data);
     }
 
-    #[Route('/nurse', name: 'app_nurse_filter', methods: ['GET'])]
+    #[Route('/nurse/filter', name: 'app_nurse_filter', methods: ['GET'])]
     public function filterByName(Request $request): JsonResponse
     {
+        $json_nurse = file_get_contents('DATA.json');
+        $json_data = json_decode($json_nurse, true);
+    
         // Obtener el nombre del parámetro de consulta
         $name = $request->query->get('first_name');
-
-        $json_nurse = file_get_contents('C:\Users\VíctorManuelLucumíGr\Documents\Symfony\ejemple_projecte\src\Controller\DATA.json');
-        $json_data = json_decode($json_nurse, true);
-
-        // Filtrar los enfermeros por nombre
-        $filter_nurses = array_filter($json_data, function($nurse) use ($name) {
+    
+        // Filtrar los enfermeros por nombre usando array_filter
+        $filtered_nurses = array_filter($json_data, function($nurse) use ($name) {
             return strtolower($nurse['first_name']) === strtolower($name);
         });
-
-        return new JsonResponse(array_values($filter_nurses));
+    
+        // Retornar los enfermeros filtrados como respuesta JSON
+        return new JsonResponse(array_values($filtered_nurses));
     }
 }
